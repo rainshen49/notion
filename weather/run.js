e08f27936918e63ac5251d3c2a003d3499150c7b
Lingkai <slk49@live.cn>
2024-01-28 16:44:11 -0500

    Change to server side rendering

--- weather/run.js
+++ weather/run.js
@@ -1,12 +1,29 @@
-import { writeFile } from "fs/promises"
+import { writeFile, readFile } from "fs/promises"
 import { fileURLToPath } from 'url'
 import path from 'node:path';
 
+function getIconUrl(code) {
+    return `https://weather.gc.ca/weathericons/${code}.gif`
+}
+
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
-await fetch("https://weather.gc.ca/api/app/en/Location/ON-143?type=city",
+const forecasts = await fetch("https://weather.gc.ca/api/app/en/Location/ON-143?type=city",
     { mode: "cors" })
-    .then(res => res.json()).then(res => res[0]["dailyFcst"]["daily"])
-    .then(forecast => {
-        writeFile(path.join(dirname, "weather.json"), JSON.stringify(forecast, null, 2))
-    });
+    .then(res => res.json()).then(res => res[0]["dailyFcst"]["daily"]);
+const indexHtml = await readFile(path.join(dirname, "index.tmpl.html"));
+
+const saturdays = forecasts.filter(d => d.date.startsWith("Sat"))
+const content = saturdays.map(day => `
+<tr>
+    <th class="date">${day["periodLabel"]}</th>
+    <td class="summary">${day["summary"]}</td>
+    <td class="temp">${day["temperature"]["periodHigh"] ?? day["temperature"]["periodLow"]} ℃
+    </td>
+    <td class="icon">
+    <img src="${getIconUrl(day["iconCode"])}"></img>
+    </td>
+<tr>`).join("\n")
+
+const output = indexHtml.toString().replace(`<!-- Content here -->`, content)
+await writeFile(path.join(dirname, "index.html"), output)
