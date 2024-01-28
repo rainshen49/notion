d9695c800c35880c6d4ff06a56aecfe6f91bf0b9
Lingkai <slk49@live.cn>
2024-01-28 16:08:29 -0500

    Integrate with Github actions

--- weather/run.js
+++ weather/run.js
@@ -1,8 +1,12 @@
 import { writeFile } from "fs/promises"
+import { fileURLToPath } from 'url'
+import path from 'node:path';
 
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 await fetch("https://weather.gc.ca/api/app/en/Location/ON-143?type=city",
     { mode: "cors" })
     .then(res => res.json()).then(res => res[0]["dailyFcst"]["daily"])
     .then(forecast => {
-        writeFile("weather.json", JSON.stringify(forecast, null, 2))
+        writeFile(path.join(dirname, "weather.json"), JSON.stringify(forecast, null, 2))
     });
